95c1c6405bcfc37c11efe4da98f204d827316831
karanmanglani <bkn45for@gmail.com>
2024-10-21 02:13:07 +0530

    Strange Partition

--- Codeforces/Practice/CP 31/900/1471A.cpp
+++ Codeforces/Practice/CP 31/900/1471A.cpp
@@ -10,14 +10,12 @@ int main() {
         cin >> n >> x;
         int mx = 0, mn = 0;
         vector<lli> a(n);
-        unordered_map<int,int> y;
         for(int i = 0;i < n;i++){
             cin >> a[i];
             mx += (a[i] % x == 0) ? a[i]/x : a[i]/x + 1;
             mn += a[i];
-            y[a[i] % x]++;
         }
-        mn = (mn%x == 0) ? mn/x : mx/x + 1;
+        mn = (mn%x == 0) ? mn/x : mn/x + 1;
         cout << mn << " " << mx<< endl;
     }
 }
