a91426d6d7d7afe96f17a338addca7c699b4d5f9
karanmanglani <bkn45for@gmail.com>
2024-10-20 23:36:57 +0530

    Exciting Bets

---
+++ Codeforces/Practice/CP 31/900/1537B.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int lli;
+
+int main() {
+    lli t;
+    cin >> t;
+    while(t--) {
+        lli n, m, x, y;
+        cin >> n >> m >> x >> y;
+        cout << "1 1 " << n << " " << m << endl;
+    }
+}
