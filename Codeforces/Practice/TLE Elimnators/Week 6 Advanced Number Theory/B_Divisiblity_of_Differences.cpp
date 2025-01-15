5878590bd07bb8a428d3b601eaf4a890f891f948
karanmanglani <bkn45for@gmail.com>
2025-01-16 01:38:16 +0530

    TLE L3 Week 6 Day 1

---
+++ Codeforces/Practice/TLE Elimnators/Week 6 Advanced Number Theory/B_Divisiblity_of_Differences.cpp
@@ -0,0 +1,158 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int lli;
+typedef long double ld;
+#define endl '\n';
+#define MOD 1000000007
+
+lli nCr(lli n, lli r) { // TC -> O(r)
+    if (r > n) return 0;
+    if (r == 0 || r == n) return 1;
+    if (r > n - r) r = n - r;
+    lli result = 1;
+    for (lli i = 0; i < r; ++i) {
+        result *= (n - i);
+        result /= (i + 1);
+    }
+    return result;
+}
+
+lli nPr(lli n, lli r) { // TC -> O(r)
+    if (r > n) return 0;
+    lli res = 1;
+    for (lli i = 0; i < r; ++i) {
+        res *= (n - i);
+    }
+    return res;
+}
+
+lli be(lli n, lli p) { // TC -> O(log(p))
+    lli x = n;
+    lli ans = 1;
+    while(p > 0) {
+        if(p & 1) {
+            ans = ans * x;
+        }
+        x *= x;
+        p >>= 1;
+    }
+    return ans;
+}
+
+lli bem(lli n, lli p) { // TC -> O(log(p % (MOD - 1)))
+    p = p % (MOD - 1);
+    lli x = n;
+    lli ans = 1;
+    while(p > 0) {
+        if(p & 1) {
+            ans = ((ans % MOD) * (x % MOD)) % MOD;
+        }
+        x = (x * x) % MOD;
+        p >>= 1;
+    }
+    return ans;
+}
+
+pair<vector<bool>, vector<lli>> primeArr(lli n) { // TC -> O(NloglogN)
+    vector<bool> arr(n + 1, true);
+    vector<lli> spf(n + 1, n);
+    spf[1] = 1;
+    arr[0] = false;
+    arr[1] = false;
+    for(lli i = 2; i * i <= n; i++) {
+        if(arr[i]) {
+            spf[i] = i;
+            for(lli j = i * i; j <= n; j += i) {
+                spf[j] = min(i, spf[j]);
+                arr[j] = false;
+            }
+        }
+    }
+    return {arr, spf};
+}
+
+map<lli, lli> pfs(lli n, vector<lli> spf) { // TC -> O(log n)
+    map<lli, lli> m;
+    while(n != 1) {
+        m[spf[n]]++;
+        n /= spf[n];
+    }
+    return m;
+}
+
+map<lli, lli> pf(lli n) { // TC -> O(sqrt(n) + log(n))
+    map<lli, lli> m;
+    for(lli i = 2; i * i <= n; i++) {
+        while(n % i == 0) {
+            m[i]++;
+            n /= i;
+        }
+    }
+    if(n > 1) m[n]++;
+    return m;
+}
+
+vector<lli> ptn(lli n) { // TC -> O(NloglogN)
+    vector<lli> v;
+    vector<bool> ip(n + 1, true);
+    ip[0] = false;
+    ip[1] = false;
+    for(lli i = 2; i * i <= n; i++) {
+        if(ip[i]) {
+            v.push_back(i);
+            for(lli j = i * i; j <= n; j += i) {
+                ip[j] = false;
+            }
+        }
+    }
+    return v;
+}
+
+lli mi(lli x) {
+    return bem(x, MOD - 2);
+}
+
+// **********************************************************************************************
+// Author : Karan Manglani
+// College: NIT Raipur
+// Function Description
+// be : Binary Exponentiation
+// bem: Binary Exponentiation including Modulus
+// primeArr: Returns {vector<bool> for seeing if a number is prime , vector<lli> spf}
+// pfs: Prime Factorisation using SPF
+// pf: Prime factorisation normally
+// ptn: retrns a vector<lli> of primes till n
+// mi: return mod inverse
+// **********************************************************************************************
+
+int main() {
+    // your code goes here
+    // lli t; cin >> t;
+    lli t = 1;
+    while(t--) {
+        lli n,k,m;cin >> n >>k >>m;
+        map<lli,multiset<lli>> mp;
+        vector<lli> a(n);for(auto &i: a) {cin >> i;mp[i%m].insert(i);}
+        bool ans = false;
+        for(auto i : mp){
+            if(i.second.size() >= k){
+                cout << "Yes" << endl;
+                ans = true;
+                vector<lli> arr(k);
+                auto x = i.second;
+                for(lli j = 0; j < k; j++) {
+                    arr[j] = *x.begin();
+                    x.erase(x.begin());
+                }
+                for(auto j : arr){
+                    cout << j << " ";
+                }
+                cout << endl;
+            }
+            if(ans) break;
+        }
+        if(!ans) cout << "No" << endl;
+    }
+    return 0;
+}
